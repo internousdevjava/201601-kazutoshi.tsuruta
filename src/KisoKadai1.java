d7a10ed2744165a2da69e700538f149365fc82de
Tsuruta <choozube@yahoo.co.jp>
2016-01-26 10:12:15 +0900

    KisoKadai1 Kisokadai2上げます。

---
+++ src/KisoKadai1.java
@@ -0,0 +1,32 @@
+/**
+ *
+ */
+
+/**
+ * @author internous
+ *
+ */
+public class KisoKadai1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int x=1;
+		int y=1;
+for(int j = 1; j <= x; j ++) {
+for(int i = 1; i <= y; i ++) {
+	int value = i * j;
+	System.out.print(j + "*" + i + "=");
+		if(value < 10) {
+		System.out.print(" ");
+		}
+		System.out.print(value + "  ");
+			}
+			 System.out.println();
+			}
+
+	}
+
+}
---
+++ src/KisoKadai2.java
@@ -0,0 +1,31 @@
+/**
+ *
+ */
+
+/**
+ * @author internous
+ *
+ */
+import java.util.Scanner;
+public class KisoKadai2 {
+
+	/**
+	 * @param args
+	 */
+
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("繰り返し入れろ");
+
+		while (sc.hasNext()){
+		String s = sc.next();
+		System.out.println("入力された文字:"+s);
+
+
+		}
+	}
+
+}
