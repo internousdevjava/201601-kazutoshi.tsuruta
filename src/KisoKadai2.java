41531bea236ab0f120d0969f17acf5149f41660e
Tsuruta <choozube@yahoo.co.jp>
2016-01-26 17:44:58 +0900

    KisoKadai2修正しました。

--- src/KisoKadai2.java
+++ src/KisoKadai2.java
@@ -1,3 +1,5 @@
+import java.io.IOException;
+
 /**
  *
  */
@@ -11,23 +13,36 @@ public class KisoKadai2 {
 
 	/**
 	 * @param args
+	 * @throws IOException
 	 */
 
 
-	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-System.out.println("数字を入力してください。");
-		int number=new java.util.Scanner(System.in) .nextInt();
+	public static void main(String[] args) throws IOException{
+		System.out.println("スタート");
+		System.out.println("数字を入力してください \n ");
 
-		   if(number==17){
-			   System.out.println("正解");
-		   }else if (number>18){
-			   System.out.println("ハズレ");
-		   }else if (number<16){
-			   System.out.println("ハズレ");
-		   }
+		int ans = new java.util.Random() .nextInt(100);
+			for(int i = 1; i <= 100; i++){
 
 
+				int input =new java.util.Scanner(System.in) .nextInt();
+;
+
+
+				if(ans == input){
+					System.out.println("True!");
+					break;
+				}else if(ans != input){
+					System.out.println("False...");
+				}
+
+				if(input > ans){
+					System.out.println("ヒント：答えは入力した数より小さいです");
+				}else if(input < ans){
+					System.out.println("ヒント：答えは入力した数より大きいです");
+				}
+
+
+			}
 	}
-
 }
